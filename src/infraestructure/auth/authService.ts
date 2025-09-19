b2873ad43e3e8423bd5f220889798cca4a343057
IasmimCristina <iasmim.cristina@codeminer42.com>
2025-09-19 10:58:35 -0300

    feat: login & logou withou login modal refactor.

---
+++ src/infraestructure/auth/authService.ts
@@ -0,0 +1,8 @@
+import type { User } from "../../domain/entities/User";
+
+export function login(name: string, password: string): User | null {
+  if (password === "password" && name.trim()) {
+    return { name };
+  }
+  return null;
+}
--- src/view/components/Hero.tsx
+++ src/view/components/Hero.tsx
@@ -1,12 +1,19 @@
-export const Hero = () => (
-  <section
-    className="relative w-full h-20 md:h-40  flex items-center justify-center overflow-hidden bg-[url('/background.jpeg')] bg-cover bg-center"
-  >
-    <div className="absolute inset-0 bg-black/60" />
-    <div className="relative z-10 flex flex-col items-center">
-      <h2 className="text-xl md:text-3xl font-bold text-white text-center drop-shadow mb-2 px-2 md:px-4">
-        Seu app que reúne receitas de diversos sabores de pizza!
-      </h2>
-    </div>
-  </section>
-);
+import { useAuth } from "../context/AuthContext";
+
+export const Hero = () => {
+  const { user } = useAuth();
+
+  return (
+    <section className="relative w-full h-20 md:h-40  flex items-center justify-center overflow-hidden bg-[url('/background.jpeg')] bg-cover bg-center">
+      <div className="absolute inset-0 bg-black/60 " />
+      <div className="relative z-10 flex flex-col items-center">
+        <h1 className="text-xl md:text-3xl">
+          {user && `Boas vindas, ${user.name}! `}
+        </h1>
+        <h2 className="text-xl md:text-3xl font-bold text-white text-center drop-shadow mb-2 px-2 md:px-4">
+          Seu app que reúne receitas de diversos sabores de pizza!
+        </h2>
+      </div>
+    </section>
+  );
+};

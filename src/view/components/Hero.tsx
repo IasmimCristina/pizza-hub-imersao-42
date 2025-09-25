fffaab10ee2144c89558f38910a8f75c0af39172
Iasmim Cristina <iaasmimcristinaa@gmail.com>
2025-09-25 14:01:49 -0300

    refactor: unused import..

--- src/view/components/Hero.tsx
+++ src/view/components/Hero.tsx
@@ -1,15 +1,9 @@
-import { useAuth } from "../context/AuthContext";
-
 export const Hero = () => {
-
-
   return (
     <section className="relative w-full h-20 md:h-40  flex items-center justify-center overflow-hidden bg-[url('/background.jpeg')] bg-cover bg-center">
       <div className="absolute inset-0 bg-black/60" />
       <div className="relative z-10 flex flex-col items-center">
-        <h1 className="text-base md:text-3xl">
-         
-        </h1>
+        <h1 className="text-base md:text-3xl"></h1>
         <h2 className="text-sm md:text-3xl font-bold text-white text-center drop-shadow mb-2 px-2 md:px-4">
           Seu app que reúne receitas de diversos sabores de pizza!
         </h2>

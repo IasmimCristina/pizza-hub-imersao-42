b610b8ea09e0415bfbc4c3a2ae183efde068b703
Iasmim Cristina <iaasmimcristinaa@gmail.com>
2025-09-25 00:28:04 -0300

    refactor: no dynamic recipe grid button select.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,15 @@
-import { useState } from "react";
-import recipesData from "./infraestructure/data/recipes.json";
-import type { Recipe } from "./domain/entities/Recipe";
 import { Header } from "./view/components/Header";
-import { RecipeGrid } from "./view/components/RecipeGrid";
+
 import { Hero } from "./view/components/Hero";
-import { RecipeDetail } from "./view/components/RecipeDetail";
 
 export const App = () => {
-  // Uso de Hooks no topo.
-  // Estado dinâmico que controla a aparição dos componentes:
-  const [selected, setSelected] = useState<Recipe | null>(null); // Uso de null para representar nenhum valor.
-  const recipes = recipesData as Recipe[];
-
   return (
     <>
       <Header />
       <main className="pt-14 md:pt-24">
         <Hero />
         <div className="px-4 md:px-8">
-          {!selected ? (
-            <section>
-              <RecipeGrid recipes={recipes} onView={setSelected} />
-            </section>
-          ) : (
-            <RecipeDetail recipe={selected} onBack={() => setSelected(null)} />
-          )}
+          <section>{/*  Grid de receitas aqui. */}</section>
         </div>
       </main>
     </>

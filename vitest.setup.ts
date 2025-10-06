0f7804d82724d780b79eba9da365e2d52b97e242
Iasmim Cristina <iaasmimcristinaa@gmail.com>
2025-10-06 16:19:08 -0300

    fix: tests - removal of cleanup.

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,11 +1,5 @@
-import { expect, afterEach } from 'vitest';
-import { cleanup } from '@testing-library/react';
-import * as matchers from '@testing-library/jest-dom/matchers';
+import { expect } from "vitest";
+import * as matchers from "@testing-library/jest-dom/matchers";
 
 // Matchers:
 expect.extend(matchers);
-
-// Limpeza automática:
-afterEach(() => {
-  cleanup();
-});
